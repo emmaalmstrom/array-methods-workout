900dae2a3cbca3c7145df90041285fa65af33626
Emma Almström <emma.almstrom@skola.botkyrka.se>
2020-12-07 16:03:57 +0100

    push och pop

---
+++ 4_push_pop.js
@@ -0,0 +1,14 @@
+// push trycker in ett argument i en array
+// de man skriver som argument i funktionen läggs längst bak i arrayen
+
+// ex: 
+const agesInTheFamily = [52, 52, 24, 21, 17]
+agesInTheFamily.push(0)
+// någon föddes, ett till syskon!
+console.log(agesInTheFamily)
+
+
+// pop tar bort det sista elementet i en funktion
+agesInTheFamily.pop()
+// bebisen dog ); 
+console.log(agesInTheFamily)
