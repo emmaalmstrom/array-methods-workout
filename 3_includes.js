b618b1a95536ac1c4f72b0a5cd5b9fe377ad82a5
Emma Almström <emma.almstrom@skola.botkyrka.se>
2020-12-07 15:55:17 +0100

    includes

---
+++ 3_includes.js
@@ -0,0 +1,11 @@
+// en inbyygd funktion som taar en array och något mer, ett nummer eller en sträng,
+// den chechar om arrayen innehåller 'något mer' 
+//och returnerar en bolean, alltså true eller false
+
+// ex:
+
+const spices = ['basilika', 'curry', 'kardemumma', 'saffran']
+
+console.log(spices.includes('basilika'))  // true
+console.log(spices.includes('spiskummin'))  // false
+console.log(spices.includes('ingefära'))  // false
