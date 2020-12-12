217f074291c6ac7ec67389125ca5d20e53154086
Emma Almström <emma.almstrom@skola.botkyrka.se>
2020-12-12 12:59:25 +0100

    some

---
+++ 5_some.js
@@ -0,0 +1,13 @@
+// some tar en funktion som ett argumment och denna function
+// ska vara ett vilkor som tar ett argument och svara med en boolean
+// beroende på om argumentet uppfyller vilkoret eller ej
+// some svarar med en boolean värde beroende på om minst ett element
+// i funtionen uppfyller vilkoret som funktionen har
+
+const arr = [1, 7, 5, 9, 33]
+const arr2 = [3, 5, 1, 6]
+
+const moreThanTen = (x) => x > 10
+
+console.log(arr.some(moreThanTen)) //true
+console.log(arr2.some(moreThanTen)) // false
